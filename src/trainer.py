9d41cb478de2b146cb56c12b1d87c667b686d702
yolanda10202 <yolanda10202@gmail.com>
2023-03-08 15:33:08 -0800

    add skeleton

--- src/trainer.py
+++ src/trainer.py
@@ -3,10 +3,29 @@
 #   def validation
 #   def kfold_cross_validation?
 
-import torch
-import numpy as np
+import torch.optim as optim
+import torch.nn as nn
 
-from src.model import #??? our class in model.py
-from src.dataloader import EEGData
+class RNNTrainer:
+    def __init__(self, rnn, learning_rate):
+        self.rnn = rnn
+        self.learning_rate = learning_rate
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.rnn.parameters(), lr=self.learning_rate)
 
-class Trainer():
+    def train(self, train_loader, num_epochs):
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            for i, data in enumerate(train_loader, 0):
+                inputs, labels = data
+                self.optimizer.zero_grad()
+
+                outputs = self.rnn(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                running_loss += loss.item()
+
+            print('Epoch: %d | Loss: %.4f' % (epoch+1, running_loss/len(train_loader)))
+
